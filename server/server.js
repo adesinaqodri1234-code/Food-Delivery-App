594730bae30db0b63579a48769238c7ed28e36b9
SQ Humble <adesinaqodri1234@gmail.com>
2026-07-21 03:47:34 -0700

    Enable CORS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5174;
